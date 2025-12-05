8784ad62e02fec692580648ab0e3b2486f65c259
ibrahimnavadia <faisalnavadia3@gmail.com>
2025-12-06 02:10:45 +0500

    updated version

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var userName = prompt("enter your name");
+console.log(userName);
+
+
 var age = +prompt("enter your age");
+console.log(age);
 
 alert("Congratulations! Happy Birthday" + " " + userName + " " + "your age is" + " " + age);
 
